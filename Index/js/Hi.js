741a136e5da7e63edc324d5ef09e55bbdab25331
Lewd for live 💯 <thejk234@gmail.com>
2022-11-10 10:21:12 -0600

    this is update

---
+++ Index/js/Hi.js
@@ -0,0 +1,10 @@
+function hoverChange_color(){
+    document.body.style.backgroundColor = "blue"
+    x = document.getElementById("fifthelm")
+    x.innerText = "5th elm modifyed"
+    console.log(x)
+
+
+}
+
+console.log(document.body.style)
